283e7f3cbf8185f821ca4023421ad85f1b7e28f6
Sahil Sharma <sahilwep@gmail.com>
2025-09-05 16:58:41 +0530

    Go: Strings

---
+++ Go-toutrial/Beginners/Strings/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "My name is Sahil." // string data type is used to store sequence of character.
+	intro := "I love golang, because it's very similar to C/C++ & C was my first love"
+	fmt.Println(name, intro)
+}
